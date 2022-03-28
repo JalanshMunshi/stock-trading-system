dd9a7783ee5aaf992d96b8064ade23d0d5fce022
JalanshMunshi <jalansh.munshi1366@gmail.com>
2022-03-28 11:24:50 -0700

    Change market schedule.

--- client/src/components/MarketSchedule/index.tsx
+++ client/src/components/MarketSchedule/index.tsx
@@ -1,18 +1,75 @@
-import React from 'react';
-import { Form, Input, Button, Typography, message, Checkbox } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Form, Input, Button, Typography, message, Checkbox, Select } from 'antd';
+import axios from 'axios';
+import { URL_PREFIX } from '../../App';
 
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
+const { Option } = Select;
+
+interface Week {
+  Monday: boolean;
+  Tuesday: boolean;
+  Wednesday: boolean;
+  Thursday: boolean;
+  Friday: boolean;
+  Saturday: boolean;
+  Sunday: boolean;
+}
 
 const MarketSchedule = () => {
   const [form] = Form.useForm();
+  const [schedule, setSchedule] = useState<Week | undefined>(undefined);
+  const days = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+  ];
 
+  useEffect(() => {
+    if(schedule === undefined) {
+      axios.get(`${URL_PREFIX}/markets/get-market-schedule`)
+          .then(res => {
+            const data = res.data.marketSchedule;
+            setSchedule(data);
+          }).catch(err => {
+            message.error('Unable to fetch market schedule.');
+            console.log(err);
+          });
+    }
+  })
+  
   const changeSchedule = (formData: any) => {
-    console.log(formData)
+    // console.log(formData);
+    var requestData = [{}];
+    days.forEach(day => {
+      if(formData[day] === false || formData[day] === undefined) {
+        requestData.push({
+          day: day,
+          running: false,
+        });
+      } else {
+        requestData.push({
+          day: day,
+          running: true,
+        });
+      }
+    });
+    axios.post(`${URL_PREFIX}/markets/admin/change-market-schedule`, {
+      email: 'admin@stockup.com',
+      schedule: requestData,
+    })
   }
   
   return (
     <>
       <Title style={{textAlign:'left'}}>Change Market Schedule</Title>
+      <Paragraph style={{textAlign:'left'}}>
+        Please select the new schedule by ticking the checkboxes.
+      </Paragraph>
       <Form
         name="change-market-schedule"
         labelCol={{ span: 8 }}
@@ -21,25 +78,25 @@ const MarketSchedule = () => {
         form={form}
       >
         <Form.Item name="Monday" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
-          <Checkbox >Monday</Checkbox>
+          <Checkbox checked={schedule?.Monday}>Monday{console.log(schedule?.Monday)}</Checkbox>
         </Form.Item>
-        <Form.Item name="Tuesday" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
-          <Checkbox checked>Tuesday</Checkbox>
+        <Form.Item name="Tuesday" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
+          <Checkbox checked={schedule?.Tuesday}>Tuesday</Checkbox>
         </Form.Item>
-        <Form.Item name="Wednesday" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
-          <Checkbox checked>Wednesday</Checkbox>
+        <Form.Item name="Wednesday" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
+          <Checkbox checked={schedule?.Wednesday}>Wednesday</Checkbox>
         </Form.Item>
-        <Form.Item name="Thursday" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
-          <Checkbox checked>Thursday</Checkbox>
+        <Form.Item name="Thursday" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
+          <Checkbox checked={schedule?.Thursday}>Thursday</Checkbox>
         </Form.Item>
-        <Form.Item name="Friday" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
-          <Checkbox checked>Friday</Checkbox>
+        <Form.Item name="Friday" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
+          <Checkbox checked={schedule?.Friday}>Friday</Checkbox>
         </Form.Item>
-        <Form.Item name="Saturday" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
-          <Checkbox>Saturday</Checkbox>
+        <Form.Item name="Saturday" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
+          <Checkbox checked={schedule?.Saturday}>Saturday</Checkbox>
         </Form.Item>
-        <Form.Item name="Sunday" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
-          <Checkbox>Sunday</Checkbox>
+        <Form.Item name="Sunday" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }} style={{width:'10%'}}>
+          <Checkbox checked={schedule?.Sunday}>Sunday</Checkbox>
         </Form.Item>
         <Form.Item 
           wrapperCol={{ offset: 8, span: 16 }}
--- server/api/markets.js
+++ server/api/markets.js
@@ -25,6 +25,38 @@ router.get('/get-market-hours', async(req, res, next) => {
     } catch (err) {
         next(err);
     }
+});
+
+router.get('/get-market-schedule', async(req, res, next) => {
+    try {
+        var marketSchedule = {};
+        await MarketSchedule.findAll({
+            attributes: [
+                'Monday',
+                'Tuesday',
+                'Wednesday',
+                'Thursday',
+                'Friday',
+                'Saturday',
+                'Sunday'
+            ]
+        }).then(data => {
+            if(data.length) {
+                marketSchedule = data[0].dataValues;
+            }
+        });
+        if(marketSchedule === {}) {
+            res.status(500).send({
+                message: 'Error in getting market hours.',
+                code: 500,
+            });
+        }
+        res.status(200).send({
+            marketSchedule
+        });
+    } catch (err) {
+        next(err);
+    }
 })
 
 // **************ADMIN ACIONS****************
@@ -42,30 +74,20 @@ router.post('/admin/change-market-hours', async (req, res, next) => {
         }).then(data => {
             user = data.dataValues;
         });
-        // console.log(user);
         if(user === null || user.role !== 'admin') {
             res.status(401).json({
                 message: 'You are not authorized to change market hours.',
                 code: 401,
             });
         }
-        // var marketHours;
         const newStartTime = req.body.startTime;
         const newEndTime = req.body.endTime;
         const marketHours = await MarketHours.findByPk(1);
-        // .then(data => {
-        //     let newHours = {
-        //         startTime: newStartTime,
-        //         endTime: newEndTime
-        //     };
-        //     data.update(newHours);
-        // });
         var start = new Date(newStartTime);
-        // console.log(start.toLocaleTimeString('it-IT'));
         var end = new Date(newEndTime);
         marketHours.startTime = start.toLocaleTimeString('it-IT');
         marketHours.endTime = end.toLocaleTimeString('it-IT');
-        console.log(marketHours);
+        // console.log(marketHours);
         await marketHours.save();
         res.status(200).json({
             message: 'Market hours changed successfully.',
@@ -81,24 +103,29 @@ router.post('/admin/change-market-schedule', async (req, res, next) => {
     // change market schedule
     try {
         const email = req.body.email;
-        const user = User.findOne({
+        var user;
+        await User.findOne({
             where: {
                 email: email,
             }
+        }).then(data => {
+            user = data.dataValues;
         });
-        if(user.role !== 'admin') {
+        if(user === null || user.role !== 'admin') {
             res.status(401).json({
-                message: 'You are not authorized to change market hours.',
+                message: 'You are not authorized to change market schedule.',
                 code: 401,
             });
         }
-        const marketSchedule = MarketSchedule.findAll();
-        const daysToChange = req.body.dayToChange;
-        daysToChange.forEach((day, openOrNot) => {
-            console.log(day);
-        })
+        const marketSchedule = await MarketSchedule.findByPk(1);
+        const daysToChange = req.body.schedule;
+        daysToChange.forEach(obj => {
+            // console.log(obj);
+            marketSchedule[obj.day] = obj.running;
+        });
+        await marketSchedule.save();
         res.status(200).json({
-            message: 'Market hours changed successfully.',
+            message: 'Market schedule changed successfully.',
             code: 200,
         });
     } catch(err) {
